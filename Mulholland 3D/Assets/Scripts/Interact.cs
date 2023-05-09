5cbb3b61a564250ceb0f66774b0c88715b6c6127
Juan Fangano <juanyfanga@gmail.com>
2023-05-09 12:13:29 -0300

    Prototype

---
+++ Mulholland 3D/Assets/Scripts/Interact.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 50f;
+
+    private void Update()
+    {
+        Debug.DrawRay(FPCamera.transform.position, FPCamera.transform.forward * range, Color.blue);
+
+        InteractInput();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                return;
+            }
+
+            Debug.Log("I hit this thing: " + hit.transform.name);
+
+
+            GameObject target = hit.transform.gameObject;
+
+            if (target == null)
+            {
+                return;
+            }
+
+            if (target.CompareTag("Interactuable"))
+            {
+                // Poner el código que queramos que le pase al target que hayamos tocado
+                // Por ahora se desactiva y ya está
+                target.SetActive(false);
+            }
+            
+
+        }
+
+        else
+        {
+            return;
+        }
+    }
+
+    private void InteractInput()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ProcessRaycast();
+        }
+    }
+}
