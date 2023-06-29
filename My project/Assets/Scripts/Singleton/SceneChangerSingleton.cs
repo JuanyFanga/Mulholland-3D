daa1ee4d8e1d8da70ccbd84dd6b9a44190d05927
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-06-29 02:36:07 -0300

    123456

--- My project/Assets/Scripts/Singleton/SceneChangerSingleton.cs
+++ My project/Assets/Scripts/Singleton/SceneChangerSingleton.cs
@@ -33,6 +33,35 @@ public class SceneChangerSingleton : MonoBehaviour
         {
             ChangeScene();
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneManager.LoadScene(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            SceneManager.LoadScene(6);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     public void ChangeScene()
