68fd300856ae1d39db990a2a6c80739cddfa8e81
Juan Fangano <juanyfanga@gmail.com>
2023-06-29 02:26:23 -0300

    SceneChanger Singleton

---
+++ My project/Assets/Scripts/Singleton/SceneChangerSingleton.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerSingleton : MonoBehaviour
+{
+    public static SceneChangerSingleton Instance { get; private set; }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        ChangeSceneToDebug();
+    }
+
+    private void ChangeSceneToDebug()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangeScene();
+        }
+    }
+
+    public void ChangeScene()
+    {
+        int actualScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = actualScene + 1;
+
+        Debug.Log("Next Scene is: " + nextScene);
+        Debug.Log("Total of scenes is: " + SceneManager.sceneCountInBuildSettings);
+
+        if (nextScene >= SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        else
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
