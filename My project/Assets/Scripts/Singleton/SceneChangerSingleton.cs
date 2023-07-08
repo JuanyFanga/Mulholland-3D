474e18430e36f25f03ddd85c80cf47d5f85b5c06
Juan Fangano <juanyfanga@gmail.com>
2023-07-08 20:11:04 -0300

    SceneChanger eliminated

--- My project/Assets/Scripts/Singleton/SceneChangerSingleton.cs
+++ My project/Assets/Scripts/Singleton/SceneChangerSingleton.cs
@@ -5,81 +5,81 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangerSingleton : MonoBehaviour
 {
-    public static SceneChangerSingleton Instance { get; private set; }
-    private void Awake()
-    {
-        // If there is an instance, and it's not me, delete myself.
+    //public static SceneChangerSingleton Instance { get; private set; }
+    //private void Awake()
+    //{
+    //    // If there is an instance, and it's not me, delete myself.
 
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
+    //    if (Instance == null)
+    //    {
+    //        Instance = this;
+    //        DontDestroyOnLoad(gameObject);
+    //    }
 
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+    //    else
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
 
-    private void Update()
-    {
-        ChangeSceneToDebug();
-    }
+    //private void Update()
+    //{
+    //    ChangeSceneToDebug();
+    //}
 
-    private void ChangeSceneToDebug()
-    {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            ChangeScene();
-        }
+    //private void ChangeSceneToDebug()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.P))
+    //    {
+    //        ChangeScene();
+    //    }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SceneManager.LoadScene(1);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SceneManager.LoadScene(2);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            SceneManager.LoadScene(3);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            SceneManager.LoadScene(4);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            SceneManager.LoadScene(5);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            SceneManager.LoadScene(6);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            SceneManager.LoadScene(0);
-        }
-    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha1))
+    //    {
+    //        SceneManager.LoadScene(1);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha2))
+    //    {
+    //        SceneManager.LoadScene(2);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha3))
+    //    {
+    //        SceneManager.LoadScene(3);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha4))
+    //    {
+    //        SceneManager.LoadScene(4);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha5))
+    //    {
+    //        SceneManager.LoadScene(5);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha6))
+    //    {
+    //        SceneManager.LoadScene(6);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha0))
+    //    {
+    //        SceneManager.LoadScene(0);
+    //    }
+    //}
 
-    public void ChangeScene()
-    {
-        int actualScene = SceneManager.GetActiveScene().buildIndex;
-        int nextScene = actualScene + 1;
+    //public void ChangeScene()
+    //{
+    //    int actualScene = SceneManager.GetActiveScene().buildIndex;
+    //    int nextScene = actualScene + 1;
 
-        Debug.Log("Next Scene is: " + nextScene);
-        Debug.Log("Total of scenes is: " + SceneManager.sceneCountInBuildSettings);
+    //    Debug.Log("Next Scene is: " + nextScene);
+    //    Debug.Log("Total of scenes is: " + SceneManager.sceneCountInBuildSettings);
 
-        if (nextScene >= SceneManager.sceneCountInBuildSettings)
-        {
-            SceneManager.LoadScene(0);
-        }
+    //    if (nextScene >= SceneManager.sceneCountInBuildSettings)
+    //    {
+    //        SceneManager.LoadScene(0);
+    //    }
 
-        else
-        {
-            SceneManager.LoadScene(nextScene);
-        }
-    }
+    //    else
+    //    {
+    //        SceneManager.LoadScene(nextScene);
+    //    }
+    //}
 }
