19f8f10171904048d5b5d23ab93ff6a81ec6f371
Juan Fangano <juanyfanga@gmail.com>
2023-09-07 16:05:59 -0300

    Chapter Selector

--- My project/Assets/Scripts/MainMenu/MainMenuInteractions.cs
+++ My project/Assets/Scripts/MainMenu/MainMenuInteractions.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuInteractions : MonoBehaviour
 {
+    [SerializeField] private GameObject[] canvas;
+
     private void Awake()
     {
         Cursor.visible = true;
@@ -18,5 +21,22 @@ public class MainMenuInteractions : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void ChapterSelector()
+    {
+        canvas[0].SetActive(false);
+        canvas[1].SetActive(true);
+    }
+
+    public void Back()
+    {
+        canvas[0].SetActive(true);
+        canvas[1].SetActive(false);
+    }
+
+    public void ChargeScene(int index)
+    {
+        SceneManager.LoadScene(index);
     }
 }
--- My project/Assets/Scripts/Singleton/SceneChangerSingleton.cs
+++ My project/Assets/Scripts/Singleton/SceneChangerSingleton.cs
@@ -5,81 +5,96 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangerSingleton : MonoBehaviour
 {
-    //public static SceneChangerSingleton Instance { get; private set; }
-    //private void Awake()
-    //{
-    //    // If there is an instance, and it's not me, delete myself.
+    public static SceneChangerSingleton Instance { get; private set; }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
 
-    //    if (Instance == null)
-    //    {
-    //        Instance = this;
-    //        DontDestroyOnLoad(gameObject);
-    //    }
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
 
-    //    else
-    //    {
-    //        Destroy(gameObject);
-    //    }
-    //}
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-    //private void Update()
-    //{
-    //    ChangeSceneToDebug();
-    //}
+    private void Update()
+    {
+        //ChangeSceneToDebug();
+        ReturnToMainMenu();
+    }
 
-    //private void ChangeSceneToDebug()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.P))
-    //    {
-    //        ChangeScene();
-    //    }
+    public void ReturnToMainMenu()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            Debug.Log("Mantaining Return");
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                Debug.Log("Back to main menu");
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
 
-    //    if (Input.GetKeyDown(KeyCode.Alpha1))
-    //    {
-    //        SceneManager.LoadScene(1);
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha2))
-    //    {
-    //        SceneManager.LoadScene(2);
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha3))
-    //    {
-    //        SceneManager.LoadScene(3);
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha4))
-    //    {
-    //        SceneManager.LoadScene(4);
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha5))
-    //    {
-    //        SceneManager.LoadScene(5);
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha6))
-    //    {
-    //        SceneManager.LoadScene(6);
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha0))
-    //    {
-    //        SceneManager.LoadScene(0);
-    //    }
-    //}
+    /*
+    private void ChangeSceneToDebug()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangeScene();
+        }
 
-    //public void ChangeScene()
-    //{
-    //    int actualScene = SceneManager.GetActiveScene().buildIndex;
-    //    int nextScene = actualScene + 1;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneManager.LoadScene(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            SceneManager.LoadScene(6);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 
-    //    Debug.Log("Next Scene is: " + nextScene);
-    //    Debug.Log("Total of scenes is: " + SceneManager.sceneCountInBuildSettings);
+    public void ChangeScene()
+    {
+        int actualScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = actualScene + 1;
 
-    //    if (nextScene >= SceneManager.sceneCountInBuildSettings)
-    //    {
-    //        SceneManager.LoadScene(0);
-    //    }
+        Debug.Log("Next Scene is: " + nextScene);
+        Debug.Log("Total of scenes is: " + SceneManager.sceneCountInBuildSettings);
 
-    //    else
-    //    {
-    //        SceneManager.LoadScene(nextScene);
-    //    }
-    //}
+        if (nextScene >= SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        else
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    } */
 }
